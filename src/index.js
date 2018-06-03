baa0faec13ee0be0fdaf7c3b03abed7768f694d0
Prabhjot Singh <prabhjot@hooptech.in>
2018-06-03 16:21:22 +0530

    wip: coming soon

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+require('./css/bootstrap.min.css');
+require('./css/bootstrap-reset.css');
+require('./assets/font-awesome/css/font-awesome.css');
+require('./assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css');
+require('./css/owl.carousel.css');
+require('./css/slidebars.css');
+require('./css/style.css');
+require('./css/style-responsive.css');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
